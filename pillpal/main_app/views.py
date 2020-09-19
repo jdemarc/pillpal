e066768e8adf484cf1f21edf7a7babeed0b2572e
joseph <josephjohndemarco@gmail.com>
2020-09-19 00:19:45 -0400

    Add views.py, update urls.py

--- pillpal/main_app/views.py
+++ pillpal/main_app/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+from django.http import HttpResponse
 # Create your views here.
+def home(request):
+    return HttpResponse('<h1>Home</h1>')
