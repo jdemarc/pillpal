54e8cc8b3256f771a54ad3522491302469bf90a1
joseph <josephjohndemarco@gmail.com>
2020-09-29 22:40:56 -0400

    Add 'add prescription' button if no prescriptions are associated with user.

--- main_app/views.py
+++ main_app/views.py
@@ -237,7 +237,7 @@ def signup(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('index')
+            return redirect('main')
         else:
             error_message = 'Invalid sign up - try again.'
     form = UserCreationForm()
